4c4e980e95bb3ae31b980fb870f01dde0f845dc9
DrekiOrmur <drekiormur@gmail.com>
2025-08-14 21:33:02 -0700

    Cleanup logging

--- ScugWheel.js
+++ ScugWheel.js
@@ -1,4 +1,3 @@
-//import { defaultWheel } from '.\\wheelConfig.js';
 
 let width = "800";
 let height = "800";
@@ -76,11 +75,7 @@ const Slugcats = [
  Inv
 ]
 
-//defaultWheel is a global variable defined in wheelConfig.js
 let defaultWheelData = loadData(defaultWheel)
-
-// console.log("Did This work?")
-// console.log(defaultWheelData)
 
 let bgWheel = new Winwheel({
  'canvasId':'bg',
@@ -108,7 +103,6 @@ let theWheel = new Winwheel({
   'type': 'spinOngoing',
   'duration' : 100,
   'callbackAfter' : drawBgWheel,
- // 'callbackFinished' : scugSelection,
   'soundTrigger' : 'segment',
   'callbackSound' : spinSound
  }
@@ -125,27 +119,18 @@ audioSpin1.volume = 0.7
 audioSpin2.volume = 0.7
 
 addEventListener("load", () => {
- console.log("I did something")
 
  resizeImageSegments(theWheel);
  theWheel.startAnimation();
 
- console.log(theWheel.segments.length)
 })
 
-
-// returns either 'light' or 'dark'
-// 'light' if black text has better readability on the given background color
-// 'dark' if white text has better readability on the given background color
-// https://colorjs.io/docs/contrast#accessible-perceptual-contrast-algorithm-apca
 
 
 // --- HELPER FUNCTIONS --- //
 
 
 function loadData(data) {
- // console.log("Its Jason!")
- // console.log(data)
 
  let wheelScug = []
  let wheelBg = []
@@ -159,11 +144,6 @@ function loadData(data) {
    wheelScug.push(img)
    wheelBg.push(fill)
  }
-
- // console.log("I have made jasons")
- // console.log(wheelScug)
- // console.log(wheelBg)
-
  return {"defaultScugWheel":wheelScug, "defaultBgWheel" :  wheelBg}
 }
 
@@ -204,15 +184,9 @@ function resultSound(sound)
 
 function spinWheel()
 {
- console.log("Im Supposed to spin now")
  clicked = true;
 
  let stopAngle = calcStop(theWheel.numSegments)
-
- console.log(stopAngle)
-
- console.log("Original Animation")
- console.log(theWheel.animation)
 
  theWheel.animation.spins = 10;
  theWheel.animation.duration = 10;
@@ -224,8 +198,6 @@ function spinWheel()
  wheelSpinning = true;
  theWheel.startAnimation();
 
- console.log("Modified Animation")
- console.log(theWheel.animation)
 }
 
 function scugSelection()
@@ -235,61 +207,23 @@ function scugSelection()
   theWheel.startAnimation();
   theWheel.draw(true)
   } else {
-   // console.log("Angles")
-   // console.log(theWheel)
-   // console.log(theWheel.getIndicatedSegment())
-   // console.log(theWheel.rotationAngle)
-   // console.log(theWheel.animation.stopAngle)
    if (Math.round(Math.floor(Math.random()*10)+1) > 2) {
-    console.log("Normal")
     resultSound(1);
    } else {
-    console.log("Adjusted")
-    //theWheel.rotationAngle = selectionAdjust(theWheel.animation.stopAngle)
     theWheel.rotationAngle = selectionAdjust(theWheel.rotationAngle)
     bgWheel.rotationAngle = theWheel.rotationAngle
-    // theWheel.animation.spins = 0;
-    // theWheel.animation.duration = 0.5;
-    // theWheel.animation.easing = "Power2.easeInOut";
-    // theWheel.animation.type = "spinToStop";
-    // theWheel.animation.callbackFinished = null;
-    // theWheel.animation.stopAngle = stopAngle;
-    // theWheel.animation.repeat = 0;
-    // wheelSpinning = true;
-    // theWheel.startAnimation();
 
     theWheel.draw(true)
     bgWheel.draw(true)
    }
-   console.log("After Troll")
-   console.log(theWheel)
-   console.log(theWheel.getIndicatedSegment())
-   console.log(theWheel.rotationAngle)
-   // if (Math.floor(Math.random()*8)+1 > 1) {
-   //  resultSound(1);
-   // } else {
-   //  theWheel.animation.stopAngle = selectionAdjust(theWheel.animation.stopAngle)
-   // }
 
   wheelSpinning = false;
   let chosenScug = (theWheel.getIndicatedSegment()).image.split('\\').pop().slice(0,-4);
-  //let scugData = Slugcats.find((element) => element.image.split('\\').pop().slice(0,-4) == chosenScug)
   let scugData = getScugData(chosenScug)
   console.log(scugData)
 
   document.documentElement.style.setProperty('--scugColor',`${scugData.fillColor}`);
-  //let s = document.getElementById("result")
-  //s.innerHTML = "Selected Character is " + chosenScug;
-  //$(".scugResult").attr("src", scugData.image);
 
-  // let img = document.createElement("img");
-  // img.setAttribute("src", scugData.image)
-  // s.prepend(img)
-
-  // let image = document.getElementsByClassName("image2");
-  // image.src = scugData.image;
-
-  //$(".scugResult").attr("src", scugData.image)
 
   if(backgroundBrightness(scugData.fillColor) == 'light')
   {
@@ -308,9 +242,6 @@ function scugSelection()
   document.getElementById("overlay").style.display = "block";
   document.getElementById("result").style.display = "flex";
   document.getElementById("wheel").removeAttribute("onclick");
-  //document.getElementById("result").style.display = "inline-block";
-  console.log(chosenScug)
-  //alert("Selected Character Is" +)
  }
 }
 
@@ -319,8 +250,6 @@ function selectionAdjust(originalStop) {
  if(howBad > 10)
  {
   resultSound(2)
-  //Random number between 10 and 50 || Math.floor(Math.random() * (max - min + 1)) + min
-  //Then either make it negative or positive
   let offset = Math.round(Math.floor((Math.random()*41)+10))
   return originalStop + (Math.random() < 0.5 ? -offset : offset)
  } else
@@ -339,7 +268,6 @@ function closeResult() {
 
 function resetSpin()
 {
- //console.log(theWheel.animation)
  theWheel.animation.spins = 5;
  theWheel.animation.duration = 200;
  theWheel.animation.easing = null;
@@ -355,13 +283,11 @@ function resetSpin()
  clicked = false;
 }
 
-// 45 * num == max degree. subtract 1.
-
 function calcStop(num) {
  let angle = 360/num
  let segment = Math.floor(Math.random()*num)+1
  let a = Math.round(((angle*segment)-1) + (Math.floor((Math.random() * angle)) -1))
- //let a = Math.floor(Math.random()*359)
+
  return a
 }
 
@@ -375,24 +301,9 @@ function resizeImageSegments(wheel) {
 
  for(var i=1; i <= wheel.numSegments; i++)
  {
-  //Find Segment
-  //console.log("ScugSearch")
-   //let scugSearch = Slugcats.find((element) => element.image.split('\\').pop() == theWheel.segments[i].imgData.src.split('/').pop())
-   //console.log(scugSearch)
-
    let srcImage = imageProps(theWheel.segments[i].imgData.src)
 
-   //console.log(theWheel.segments[i].imgData.src)
-
-    
    resizeImage(srcImage, wheel.segments[i].imgData, wheel.numSegments)
-
-   // console.log("Modified imgData Data");
-   // console.log(wheel.segments[i].imgData);
-
-   //console.log("New Size")
-   //console.log(wheel.segments[i].imgData.height)
-   //console.log(wheel.segments[i].imgData.width)
  }
 }
 
@@ -400,36 +311,27 @@ function resizeImage(srcImage, segmentImage, num) {
  let maxWidth = getMaxWidth(num)
  let maxHeight = 170
 
- // console.log("Debug")
- // console.log(maxWidth)
- // console.log(srcImage.width)
-
  if(maxWidth <= srcImage.width && (maxHeight >= (srcImage.height * (maxWidth/srcImage.width))))
  {
-   //console.log("Not Wide Enough!")
    segmentImage.width = maxWidth
    segmentImage.height = srcImage.height * (maxWidth/srcImage.width)
  }
  else
  {
-   //console.log("Too Wide")
    segmentImage.width = srcImage.width * (maxHeight/srcImage.height)
    segmentImage.height = maxHeight
  }
- //console.log("Image Style")
- //console.log(segmentImage.style)
 }
 
 function imageProps(srcImage) {
  origImage = new Image()
  origImage.src = srcImage
- // console.log("Testing Images")
- // console.log(origImage.width)
- // console.log(origImage.height)
+
  return origImage
 }
 
 function getMaxWidth(num){
+ //I can't believe I tricked myself into trig
 
  const pi = Math.PI;
  let radians = Math.floor(((360/num)*pi/180)*100)
@@ -440,15 +342,7 @@ function getMaxWidth(num){
  let arcLength = (radians/100)*radius
  
  let arcChord = Math.floor(2*radius*Math.sin(arcLength/(2*radius)))
- 
- // console.log("Math Time")
- // console.log(radians)
- // console.log(w)
- // console.log(h)
- // console.log(radius)
- // console.log(arcLength)
- // console.log(arcChord)
- 
+  
  return arcChord
  }
 
@@ -461,18 +355,8 @@ function getMaxWidth(num){
  }
  
  function removeScug(scugName){
-  console.log(theWheel.segments)
-  // //let scugSearch = Slugcats.find((element) => element.image.split('\\').pop() == theWheel.segments[i].imgData.src.split('/').pop())
-  //let scugSearch = theWheel.segments.find((element) => element.image.split('\\').pop().slice(0,-4) == scugName)
-  console.log(scugName)
+  
   let scugSearch = theWheel.segments.findIndex(segment => segment != null && segment.imgData.src.split('/').pop().slice(0,-4) == scugName)
-  // for(s in theWheel.segments)
-  // {
-  //  console.log("Loop Shit")
-  //  console.log(theWheel.segments)
-  // }
-  console.log("Remove Segment")
-  console.log(scugSearch)
 
   if(scugSearch > 0)
   {
@@ -488,37 +372,29 @@ function getMaxWidth(num){
  }
 
  function addScug(scugName){
-  console.log(theWheel.segments)
-  // //let scugSearch = Slugcats.find((element) => element.image.split('\\').pop() == theWheel.segments[i].imgData.src.split('/').pop())
-  //let scugSearch = theWheel.segments.find((element) => element.image.split('\\').pop().slice(0,-4) == scugName)
-  console.log(Slugcats)
-  console.log(scugName)
-  //let scugSearch = theWheel.segments.findIndex(segment => segment != null && segment.imgData.src.split('/').pop().slice(0,-4) == scugName)
   let scugData = Slugcats.find((element) => element.image.split('\\').pop().slice(0,-4) == scugName)
-  console.log("Add Segment")
-  //console.log(thing)
-  console.log(scugData.image)
-  console.log(scugData.fillColor)
-  //console.log(scugSearch)
+  let newWheelSegment = theWheel.addSegment()
+  let bgWheelSegment = bgWheel.addSegment()
 
-   let newWheelSegment = theWheel.addSegment()
-   let bgWheelSegment = bgWheel.addSegment()
+  newWheelSegment.image = scugData.image
+  newWheelSegment.imgData = new Image();
+  newWheelSegment.imgData.onload = winwheelLoadedImage;
+  newWheelSegment.imgData.src = newWheelSegment.image
 
-   newWheelSegment.image = scugData.image
-   newWheelSegment.imgData = new Image();
-   newWheelSegment.imgData.onload = winwheelLoadedImage;
-   newWheelSegment.imgData.src = newWheelSegment.image
+  bgWheelSegment.fillStyle = scugData.fillColor
 
-   bgWheelSegment.fillStyle = scugData.fillColor
+  resizeImageSegments(theWheel)
 
-   resizeImageSegments(theWheel)
+  console.log(theWheel)
  
-   console.log(theWheel)
-  
-   theWheel.draw()
-   bgWheel.draw()
+  theWheel.draw()
+  bgWheel.draw()
  }
 
+// returns either 'light' or 'dark'
+// 'light' if black text has better readability on the given background color
+// 'dark' if white text has better readability on the given background color
+// https://colorjs.io/docs/contrast#accessible-perceptual-contrast-algorithm-apca
  function backgroundBrightness(color) {
   const backgroundColor = new Color(color)
   const blackText = new Color('#000000')
@@ -529,39 +405,3 @@ function getMaxWidth(num){
  
   return darkContrast > lightContrast ? 'dark' : 'light'
  }
-
-// --- UNUSED but I dont wanna remake --- //
-//Regex to match segment/original image for scaling on add/remove
-//const regEx = new RegExp("[\w-]+\.png");
- 
-
-// addEventListener("load", () => {
-//  console.log("Original Wheel")
-//  console.log(theWheel)
-//  console.log(theWheel.segments[8].imgData)
-//  console.log(theWheel.segments[8].imgData.src)
-//  console.log(Saint.image)
-
-//  //resizeImageSegments(theWheel)
-
-//  console.log("Modified Wheel")
-//  console.log(theWheel)
-
-//  //var ctx = document.getElementById("canvas")
-
-//  //console.log(ctx)
-
-// })
-
-
-// $(document).ready(function(){
-//  console.log("I did something")
-//  console.log(defaultWheelData)
-
-//  //initWheels();
-//  console.log(theWheel.segments)
-
-//  resizeImageSegments(theWheel);
-//  //theWheel.draw()
-//  theWheel.startAnimation();
-// });
